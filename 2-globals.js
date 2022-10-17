dd614ac070a3d76695521152c086f6955a58c496
Matthew Nolan <mnolan132@gmail.com>
2022-10-17 15:23:40 +1300

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+//GLOBALS _ NO WINDOWS!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+//module - info about current module (file)
+// process - info about enviroment where th program is being excecuted 
+
+
+console.log(__dirname)
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
